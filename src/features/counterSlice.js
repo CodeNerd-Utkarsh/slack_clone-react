d5bb582df09be54f6c20a4dd07203755cd6ccc83
Utkasrh Singh <singhutkars2721@outlook.com>
2021-06-17 10:13:14 +0530

    clean up of the project

--- src/features/counterSlice.js
+++ src/features/counterSlice.js
@@ -1,5 +1,12 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { fetchCount } from './counterAPI';
+
+
+// A mock function to mimic making an async request for data
+export function fetchCount(amount = 1) {
+  return new Promise((resolve) =>
+    setTimeout(() => resolve({ data: amount }), 500)
+  );
+}
 
 const initialState = {
   value: 0,
